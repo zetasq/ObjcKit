2600967816733d64a99fc08b3a9ea8af90dbd0e4
Zhu Shengqi <zetasq@gmail.com>
2018-02-01 09:15:04 +0800

    Initial Commit

---
+++ ObjcKit/ObjcKit.h
@@ -0,0 +1,19 @@
+//
+//  ObjcKit.h
+//  ObjcKit
+//
+//  Created by Zhu Shengqi on 01/02/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjcKit.
+FOUNDATION_EXPORT double ObjcKitVersionNumber;
+
+//! Project version string for ObjcKit.
+FOUNDATION_EXPORT const unsigned char ObjcKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcKit/PublicHeader.h>
+
+
