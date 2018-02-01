b9fbbd4049e95ab67f2b3c48aa46eba3812e0c2e
Zhu Shengqi <zetasq@gmail.com>
2018-02-01 23:30:17 +0800

    Add let and var macros

--- ObjcKit/ObjcKit.h
+++ ObjcKit/ObjcKit.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char ObjcKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <ObjcKit/PublicHeader.h>
 
 
+#import <ObjcKit/TypeInfer.h>
---
+++ ObjcKit/TypeInfer.h
@@ -0,0 +1,23 @@
+//
+//  TypeInfer.h
+//  ObjcKit
+//
+//  Created by Zhu Shengqi on 01/02/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#pragma once
+
+#if defined(__cplusplus)
+
+#define let auto const
+#define var auto
+
+#else
+
+#define let const __auto_type
+#define var __auto_type
+
+#endif
+
+
